33b47033596efb0a4c1124634b703d42afc1d079
Lam Hieu <lamhieu.vk@gmail.com>
2019-01-28 23:25:12 +0700

    fix: type of port in cmd input

--- src/cli.js
+++ src/cli.js
@@ -9,7 +9,7 @@ const { logger } = utils || {}
 program
   .version('0.1.2')
   .usage('[options] <file ...>')
-  .option('-p, --port <port>', 'server port', parseInt, 3000)
+  .option('-p, --port <port>', 'server port', Number, 3000)
   .option('-u, --url <url>', 'redirect to url', String)
   .option('-c, --cookie', 'should use cookie')
 
