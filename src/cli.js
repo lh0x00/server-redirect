37d5b0aee9de580c036900ccc6ce04aef89885a9
Lam Hieu <lamhieu.vk@gmail.com>
2019-01-29 00:02:30 +0700

    chore: update version to 0.2.0

--- src/cli.js
+++ src/cli.js
@@ -8,7 +8,7 @@ const exposer = require('./exposer')
 const { logger } = utils || {}
 
 program
-  .version('0.1.2')
+  .version('0.2.0')
   .usage('[options] <file ...>')
   .option('-p, --port <port>', 'server port', Number, 3000)
   .option('-u, --url <url>', 'redirect to url', String)
